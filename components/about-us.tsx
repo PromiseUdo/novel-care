f4b4888d934dbf0169311cc1716a010dd666d8d5
PromiseUdo <info.promiseudo@gmail.com>
2025-06-04 06:23:37 +0100

    update about us

--- components/about-us.tsx
+++ components/about-us.tsx
@@ -29,8 +29,9 @@ const AboutUs = () => {
             />
           </div>
 
-          <div className="grid grid-cols-2 items-center gap-[108px]">
-            <div className="flex flex-col gap-[24px]">
+          {/* Added min-h-[400px] to ensure flex container has enough height for items-center */}
+          <div className="flex items-center justify-between min-h-[400px] pb-[40px] gap-[108px] ">
+            <div className="w-full flex flex-col gap-[48px]">
               <p className="leading-[28px]">
                 Novel Care Services is a support service organization which was
                 established out of the desire to consistently provide
@@ -40,28 +41,56 @@ const AboutUs = () => {
               <p className="leading-[28px]">
                 Our aim is to support our clients well enough to participate
                 effectively in society and achieve their goals. We are
-                enthusiastic about providing support which is tailored
-                specifically for you and your loved ones. We are here to help
-                people with physical and mental disabilities live life to its
-                fullest. We also believe that people with disability deserve to
-                participate in the community as much as they desire.
+                enthusiastic about providing support tailored specifically for
+                you and your loved ones. We are here to help people with
+                physical and mental disabilities live life to its fullest. We
+                also believe that people with disability deserve to participate
+                in the community as much as they desire.
               </p>
             </div>
 
-            {/* 2 images */}
-            {/* Added relative positioning and dimensions to contain both images */}
-            <div className="relative  h-full">
-              {/* Bottom image with inverted D-shape */}
+            {/* Added h-full to ensure image column matches flex container height */}
+            <div className="relative w-full h-full self-stretch">
               <img
                 src="/about1.png"
                 alt="Bottom background image"
-                className="absolute top-0 left-[-50px] w-[300px] h-[325px]"
+                className="absolute top-[40px] left-[-50px] w-[300px] h-[325px]"
               />
               <img
                 src="/about2.png"
                 alt="Top background image"
-                className="absolute top-[-40px] right-[0px] w-[300px] h-[325px]"
+                className="absolute top-0 right-0 w-[300px] h-[325px]"
               />
+            </div>
+          </div>
+
+          {/* Added min-h-[400px] to ensure flex container has enough height for items-center */}
+          <div className="flex items-center justify-between min-h-[400px] gap-[108px] ">
+            <div className="relative w-full h-full self-stretch">
+              <img
+                src="/about3.png"
+                alt="Bottom background image"
+                className="absolute top-[40px] left-0 w-[300px] h-[325px]"
+              />
+              <img
+                src="/about2.png"
+                alt="Top background image"
+                className="absolute top-0 right-[-40px] w-[300px] h-[325px] scale-x-[-1]"
+              />
+            </div>
+
+            <div className="w-full flex flex-col gap-[24px]">
+              <p className="leading-[28px]">
+                Our holistic approach prioritises the involvement of all
+                stakeholders in the delivery of our services. This means that
+                listening to your opinions and concerns is a priority for us at
+                Novel Care Services. For us, excellent care means we always take
+                care to listen to you and your family and ensure that we deliver
+                the services you need exactly the way you want it. We always aim
+                to build and maintain a strong relationship with our clients and
+                their families. We, therefore, support and care for our
+                participants like family.
+              </p>
             </div>
           </div>
         </div>
