6d9bd9caa1e63fceb596de365cfa50fb8888d04c
PromiseUdo <info.promiseudo@gmail.com>
2025-06-12 01:05:32 +0100

    update

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,13 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import MaxWidthWrapper from "./max-width-wrapper";
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+import { Button } from "./ui/button";
+import { AnimatePresence, motion } from "framer-motion";
+import { cn } from "@/lib/utils";
+import { X } from "lucide-react";
 
 const navLinks = [
   { url: "/", label: "Home" },
@@ -16,8 +20,12 @@ const navLinks = [
 ];
 
 const Navbar = () => {
+  const router = useRouter();
   const pathname = usePathname(); // Get the current pathname
-
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <div className="shadow w-full h-[96px] bg-white py-[16px] sticky top-0 z-50">
       <MaxWidthWrapper>
@@ -26,27 +34,131 @@ const Navbar = () => {
             <Image src={"/logo.png"} alt="logo" fill className="" />
           </div>
 
-          <div className="flex items-center gap-[25px]">
-            {navLinks.map((link) => {
-              // Determine if the link is active
-              const isActive = link.url
-                ? pathname === link.url // Exact match for non-empty URLs
-                : false; // Links with empty URLs are never active
+          <div className="ml-auto flex items-center">
+            <div className="hidden md:flex items-center gap-[25px]">
+              {navLinks.map((link) => {
+                // Determine if the link is active
+                const isActive = link.url
+                  ? pathname === link.url // Exact match for non-empty URLs
+                  : false; // Links with empty URLs are never active
 
-              return (
-                <Link
-                  key={link.label}
-                  className={`transition-all duration-200 font-montserrat border-b-2 font-medium hover:text-[#E67817FF] hover:border-[#E67817FF] ${
-                    isActive
-                      ? "text-[#E67817FF] border-[#E67817FF]"
-                      : "border-white"
-                  }`}
-                  href={link.url}
-                >
-                  {link.label}
-                </Link>
-              );
-            })}
+                return (
+                  <Link
+                    key={link.label}
+                    className={`transition-all duration-200 font-montserrat border-b-2 font-medium hover:text-[#E67817FF] hover:border-[#E67817FF] ${
+                      isActive
+                        ? "text-[#E67817FF] border-[#E67817FF]"
+                        : "border-white"
+                    }`}
+                    href={link.url}
+                  >
+                    {link.label}
+                  </Link>
+                );
+              })}
+            </div>
+
+            {/* mobile nav */}
+
+            <div className="md:hidden">
+              <button
+                className={cn(
+                  "relative z-50 flex flex-col items-center justify-center w-10 h-10 group",
+                  isOpen === true && "hidden"
+                )}
+                onClick={toggleMenu}
+                aria-label="Toggle menu"
+              >
+                {/* Line 1 */}
+                <motion.span
+                  className="block w-6 h-0.5 bg-[#1e1e1e] transition-transform"
+                  animate={isOpen ? { rotate: 45, y: 6 } : { rotate: 0, y: 0 }}
+                  transition={{ duration: 0.1 }}
+                />
+                {/* Line 2 */}
+                <motion.span
+                  className="block w-6 h-0.5 bg-[#1e1e1e] mt-1.5 transition-transform"
+                  animate={isOpen ? { opacity: 0 } : { opacity: 1 }}
+                  transition={{ duration: 0.4 }}
+                />
+                {/* Line 3 */}
+                <motion.span
+                  className="block w-6 h-0.5 bg-[#1e1e1e] mt-1.5 transition-transform"
+                  animate={
+                    isOpen ? { rotate: -45, y: -9 } : { rotate: 0, y: 0 }
+                  }
+                  transition={{ duration: 0.1 }}
+                />
+              </button>
+              <motion.div
+                initial={{ height: 0 }}
+                animate={{ height: isOpen ? "100vh" : 0 }}
+                transition={{ duration: 0.3 }}
+                className="fixed top-0 left-0 w-full h-screen bg-white  z-40 overflow-hidden"
+              >
+                <div className="flex flex-col items-center justify-center h-full gap-[25px] text-[#1e1e1e]">
+                  <Link
+                    href="/"
+                    onClick={() => setIsOpen(false)}
+                    className="font-medium text-lg"
+                  >
+                    Home
+                  </Link>
+                  <Link
+                    href="/ndis"
+                    onClick={() => setIsOpen(false)}
+                    className="font-medium text-lg"
+                  >
+                    NDIS
+                  </Link>
+                  <Link
+                    href="/referral"
+                    onClick={() => setIsOpen(false)}
+                    className="font-medium text-lg"
+                  >
+                    Referral
+                  </Link>
+                  <Link
+                    href="/employment"
+                    onClick={() => setIsOpen(false)}
+                    className="font-medium text-lg"
+                  >
+                    Employment
+                  </Link>
+                  <Link
+                    href="/blog"
+                    onClick={() => setIsOpen(false)}
+                    className="font-medium text-lg"
+                  >
+                    Blog
+                  </Link>
+                  <Link
+                    href="/covid-19"
+                    onClick={() => setIsOpen(false)}
+                    className="font-medium text-lg"
+                  >
+                    COVID-19
+                  </Link>
+                  <Link
+                    href="/feedback"
+                    onClick={() => setIsOpen(false)}
+                    className="font-medium text-lg"
+                  >
+                    Feedback
+                  </Link>
+
+                  {/* place a close icon here from lucide react use SetisOpento close - false it */}
+
+                  <button
+                    onClick={() => setIsOpen(false)}
+                    className=" text-[#1e1e1e] z-50"
+                    aria-label="Close menu"
+                  >
+                    <X size={28} />
+                  </button>
+                </div>
+              </motion.div>
+            </div>
           </div>
         </div>
       </MaxWidthWrapper>
