2786768c53bdfe44584a9d80d34be7f92736c688
PromiseUdo <info.promiseudo@gmail.com>
2025-06-12 03:33:31 +0100

    update

--- app/employment/components/employment.tsx
+++ app/employment/components/employment.tsx
@@ -1,11 +1,14 @@
+"use client";
 import MaxWidthWrapper from "@/components/max-width-wrapper";
 import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const Employment = () => {
+  const router = useRouter();
   return (
     <MaxWidthWrapper>
-      <div className="rounded-[40px] p-[40px] w-full md:max-w-[600px] mt-16 bg-[#FAE4D180] mx-auto">
+      <div className="rounded-[40px] px-[16px] py-[40px] md:p-[40px] w-full md:max-w-[600px] mt-16 bg-[#FAE4D180] mx-auto">
         <div className="w-full flex items-center justify-center flex-col gap-[40px]">
           <h2 className="text-[40px] text-[#4E4D4D]">Employment</h2>
           <div className="flex flex-col gap-[40px]">
@@ -22,7 +25,10 @@ const Employment = () => {
               motivation at Novel Care Services.
             </p>
           </div>
-          <Button className="bg-[#E67817] text-[#0F0E0E] font-semibold w-[300px] rounded-[64px] h-[52px]">
+          <Button
+            onClick={() => router.push("/contact")}
+            className="bg-[#E67817] text-[#0F0E0E] font-semibold w-[300px] rounded-[64px] h-[52px]"
+          >
             Contact us
           </Button>
         </div>
